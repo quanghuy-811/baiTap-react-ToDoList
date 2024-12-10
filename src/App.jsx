cf5372986bb1399d411564280bbd93524b536fe4
quanghuy-811 <anhthu157.do@gmail.com>
2024-12-10 21:25:07 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p className="text-red-400">a</p>
+    </div>
+  );
+}
+
+export default App;
